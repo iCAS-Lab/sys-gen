ae8bbacfb96de817edb88c01e8d542eacc40d104
Peyton Chandarana <peytonchandarana@gmail.com>
2024-08-27 23:36:31 -0400

    Added argument parser for easy generation of verilog.

--- src/main.py
+++ src/main.py
@@ -7,13 +7,21 @@ import sys
 from pathlib import Path
 
 # Local Libs
+from utils.argparser import parse_arguments, set_config
 from utils.config import Config
 from verilog.systolic_array import SystolicArray
 from verilog.spiking_systolic_array import SpikingSystolicArray
 from verilog.activation_unit import ActivationUnit
 ################################################################################
 if __name__ == '__main__':
+    # Parse arguments passed in by the user
+    args = parse_arguments()
+
+    # Init config and set any parameters passed in by the user
     config = Config()
+    set_config(args, config)
+
+    # Build verilog
     systolic_array = SystolicArray(config=config)
     spiking_systolic_array = SpikingSystolicArray(config=config)
     activation_unit = ActivationUnit(config=config)
--- src/utils/argparser.py
+++ src/utils/argparser.py
@@ -1,6 +1,87 @@
 """Argument parser and configuration parser for building a systolic array.
 """
 ################################################################################
+# Std libs
 import argparse
+
+# Local libs
+from utils.config import Config
 ################################################################################
 ################################################################################
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        prog='Python Systolic Array Verilog Compiler',
+        description=(
+            'This program builds up and connects verilog modules '
+            + 'via Python to enable easy manipulation and generation of '
+            + 'verilog code for building systolic arrays.'
+        )
+    )
+    # Systolic array size
+    parser.add_argument(
+        '-r', '--rows',
+        type=int,
+        default=32,
+        help='Number of rows in the systolic array'
+    )
+    parser.add_argument(
+        '-c', '--cols',
+        type=int,
+        default=32,
+        help='Number of columns in the systolic array'
+    )
+    # Precision
+    parser.add_argument(
+        '-d', '--data-width',
+        type=int,
+        default=16,
+        help='Precision of integer computations'
+    )
+    parser.add_argument(
+        '-t', '--accumulate-interval-width',
+        type=int,
+        default=5,
+        help='Number of bits for the accumulation interval'
+    )
+    parser.add_argument(
+        '-f', '--fifo-depth',
+        type=int,
+        default=8,
+        help='Number of values that can be stored in the FIFOs'
+    )
+    parser.add_argument(
+        '-n', '--name',
+        type=str,
+        help='Name of the developer in verilog heading'
+    )
+    parser.add_argument(
+        '--target-device',
+        type=str,
+        help='Name of the device the verilog code is targeting'
+    )
+    parser.add_argument(
+        '--tool-version',
+        type=str,
+        help='Version of the synthesis tool being used'
+    )
+    parser.add_argument(
+        '--description',
+        type=str,
+        help='Description to add to the verilog heading comment'
+    )
+    args = parser.parse_args()
+    return args
+
+
+def set_config(args, config: Config):
+    config.ROWS = args.rows
+    config.COLS = args.cols
+    config.DATA_WIDTH = args.data_width
+    config.ACCUMULATE_TIME_WIDTH = args.accumulate_interval_width
+    config.FIFO_DEPTH = args.fifo_depth
+    config.ENGINEER = str(args.name)
+    config.TARGET_DEVICES = str(args.target_device)
+    config.TOOL_VERSION = str(args.tool_version)
+    config.DESCRIPTION = str(args.description)
