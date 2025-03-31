2ba977e396b10f87c852478aa2bfe978205676ec
Peyton Chandarana <peytonchandarana@gmail.com>
2025-03-31 18:14:43 -0400

    Fixed import.

--- src/verilog_gen/spiking/spiking.py
+++ src/verilog_gen/spiking/spiking.py
@@ -5,7 +5,7 @@
 
 # Local libs
 from utils.config import Config
-from src.utils.tcl_generator import TCLGenerator
+from utils.tcl_generator import TCLGenerator
 from verilog_gen.spiking.spiking_systolic_array import SpikingSystolicArray
 ################################################################################
 
